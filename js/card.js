efee86ecfc3daead67aae5d1a0278f33d151f9d5
nunok21 <neatnok2109@gmail.com>
2021-11-11 18:27:40 +0700

    update
    update

---
+++ js/card.js
@@ -0,0 +1,474 @@
+var card = {
+    "3020": {
+        "cardVal": "2C",
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "4020": {
+        "cardVal": "2D",
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "2020": {
+        "cardVal": "2H",
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "1020": {
+        "cardVal": "2S",
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "3030": {
+        "cardVal": "3C",
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "4030": {
+        "cardVal": "3D",
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "2030": {
+        "cardVal": "3H",
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "1030": {
+        "cardVal": "3S",
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "3040": {
+        "cardVal": "4C",
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "4040": {
+        "cardVal": "4D",
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "2040": {
+        "cardVal": "4H",
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "1040": {
+        "cardVal": "4S",
+        "baccarat": 4,
+        "dragontiger": 4
+
+    },
+    "3050": {
+        "cardVal": "5C",
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "4050": {
+        "cardVal": "5D",
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "2050": {
+        "cardVal": "5H",
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "1050": {
+        "cardVal": "5S",
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "3060": {
+        "cardVal": "6C",
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "4060": {
+        "cardVal": "6D",
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "2060": {
+        "cardVal": "6H",
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "1060": {
+        "cardVal": "6S",
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "3070": {
+        "cardVal": "7C",
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "4070": {
+        "cardVal": "7D",
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "2070": {
+        "cardVal": "7H",
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "1070": {
+        "cardVal": "7S",
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "3080": {
+        "cardVal": "8C",
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "4080": {
+        "cardVal": "8D",
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "2080": {
+        "cardVal": "8H",
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "1080": {
+        "cardVal": "8S",
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "3090": {
+        "cardVal": "9C",
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "4090": {
+        "cardVal": "9D",
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "2090": {
+        "cardVal": "9H",
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "1090": {
+        "cardVal": "9S",
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "3100": {
+        "cardVal": "10C",
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "4100": {
+        "cardVal": "10D",
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "2100": {
+        "cardVal": "10H",
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "1100": {
+        "cardVal": "10S",
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "3010": {
+        "cardVal": "AC",
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "4010": {
+        "cardVal": "AD",
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "2010": {
+        "cardVal": "AH",
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "1010": {
+        "cardVal": "AS",
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "3110": {
+        "cardVal": "JC",
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "4110": {
+        "cardVal": "JD",
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "2110": {
+        "cardVal": "JH",
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "1110": {
+        "cardVal": "JS",
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "3120": {
+        "cardVal": "QC",
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "4120": {
+        "cardVal": "QD",
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "2120": {
+        "cardVal": "QH",
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "1120": {
+        "cardVal": "QS",
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "3130": {
+        "cardVal": "KC",
+        "baccarat": 0,
+        "dragontiger": 13
+    },
+    "4130": {
+        "cardVal": "KD",
+        "baccarat": 0,
+        "dragontiger": 13
+    },
+    "2130": {
+        "cardVal": "KH",
+        "baccarat": 0,
+        "dragontiger": 13
+    },
+    "1130": {
+        "cardVal": "KS",
+        "baccarat": 0,
+        "dragontiger": 13
+    }
+};
+
+var card2 = {
+    "2C": {
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "2D": {
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "2H": {
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "2S": {
+        "baccarat": 2,
+        "dragontiger": 2
+    },
+    "3C": {
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "3D": {
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "3H": {
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "3S": {
+        "baccarat": 3,
+        "dragontiger": 3
+    },
+    "4C": {
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "4D": {
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "4H": {
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "4S": {
+        "baccarat": 4,
+        "dragontiger": 4
+    },
+    "5C": {
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "5D": {
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "5H": {
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "5S": {
+        "baccarat": 5,
+        "dragontiger": 5
+    },
+    "6C": {
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "6D": {
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "6H": {
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "6S": {
+        "baccarat": 6,
+        "dragontiger": 6
+    },
+    "7C": {
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "7D": {
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "7H": {
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "7S": {
+        "baccarat": 7,
+        "dragontiger": 7
+    },
+    "8C": {
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "8D": {
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "8H": {
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "8S": {
+        "baccarat": 8,
+        "dragontiger": 8
+    },
+    "9C": {
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "9D": {
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "9H": {
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "9S": {
+        "baccarat": 9,
+        "dragontiger": 9
+    },
+    "10C": {
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "10D": {
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "10H": {
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "10S": {
+        "baccarat": 0,
+        "dragontiger": 10
+    },
+    "AC": {
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "AD": {
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "AH": {
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "AS": {
+        "baccarat": 1,
+        "dragontiger": 1
+    },
+    "JC": {
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "JD": {
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "JH": {
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "JS": {
+        "baccarat": 0,
+        "dragontiger": 11
+    },
+    "QC": {
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "QD": {
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "QH": {
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "QS": {
+        "baccarat": 0,
+        "dragontiger": 12
+    },
+    "KC": {
+        "baccarat": 0,
+        "dragontiger": 13
+    },
+    "KD": {
+        "baccarat": 0,
+        "dragontiger": 13
+    },
+    "KH": {
+        "baccarat": 0,
+        "dragontiger": 13
+    },
+    "KS": {
+        "baccarat": 0,
+        "dragontiger": 13
+    }
+};
---
+++ js/statistic.js
@@ -0,0 +1,37 @@
+var statistic = {
+    "p": "img/Player/P.png",
+    "pp": "img/Player/PP.png",
+    "pb": "img/Player/PB.png",
+    "ppb": "img/Player/PPB.png",
+   
+    "b": "img/Banker/B.png",
+    "bp": "img/Banker/BP.png",
+    "bb": "img/Banker/BB.png",
+    "bpb": "img/Banker/BPB.png",
+
+    "t": "img/T.png",
+    "n": "img/none.png"
+};
+
+var statistic2 = {
+    "p": "img/Player/P_.png",
+    "pt": "img/Player/PT_.png",
+    "ppt": "img/Player/PPT_.png",
+    "pbt": "img/Player/PBT_.png",
+    "pp": "img/Player/PP_.png",
+    "pb": "img/Player/PB_.png",
+    "ppb": "img/Player/PPB_.png",
+    "ppbt": "img/Player/PPBT_.png",
+
+    "b": "img/Banker/B_.png",
+    "bt": "img/Banker/BT_.png",
+    "bbt": "img/Banker/BBT_.png",
+    "bpt": "img/Banker/BPT_.png",
+    "bp": "img/Banker/BP_.png",
+    "bb": "img/Banker/BB_.png",
+    "bpb": "img/Banker/BPB_.png",
+    "bpbt": "img/Banker/BPBT_.png",
+
+    "n": "img/none.png"
+};
+
